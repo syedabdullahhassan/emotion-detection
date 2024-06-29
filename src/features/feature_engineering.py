9328ccb1fd65be5b33a50b03f97542e0d4b37038
Syed Abdillah Hasan <syedabdullahhassan@yahoo.com>
2024-06-29 21:06:48 +0500

    updated feature eng file

--- src/features/feature_engineering.py
+++ src/features/feature_engineering.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import os
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 import yaml
 import logging
 
@@ -53,23 +53,23 @@ def load_data(file_path: str) -> pd.DataFrame:
         logger.error('Unexpected error occurred while loading the data: %s', e)
         raise
 
-def apply_tfidf(train_data: pd.DataFrame, test_data: pd.DataFrame, max_features: int) -> tuple:
-    """Apply TfIdf to the data."""
+def apply_bow(train_data: pd.DataFrame, test_data: pd.DataFrame, max_features: int) -> tuple:
+    """Apply bow CountVectorization to the data."""
     try:
-        vectorizer = TfidfVectorizer(max_features=max_features)
+        vectorizer = CountVectorizer(max_features=max_features)
 
         X_train = train_data['content'].values
         y_train = train_data['sentiment'].values
         X_test = test_data['content'].values
         y_test = test_data['sentiment'].values
 
-        X_train_bow = vectorizer.fit_transform(X_train)
-        X_test_bow = vectorizer.transform(X_test)
+        X_train_Tfid = vectorizer.fit_transform(X_train)
+        X_test_Tfid = vectorizer.transform(X_test)
 
-        train_df = pd.DataFrame(X_train_bow.toarray())
+        train_df = pd.DataFrame(X_train_Tfid.toarray())
         train_df['label'] = y_train
 
-        test_df = pd.DataFrame(X_test_bow.toarray())
+        test_df = pd.DataFrame(X_test_Tfid.toarray())
         test_df['label'] = y_test
 
         logger.debug('Bag of Words applied and data transformed')
@@ -96,7 +96,7 @@ def main():
         train_data = load_data('./data/interim/train_processed.csv')
         test_data = load_data('./data/interim/test_processed.csv')
 
-        train_df, test_df = apply_tfidf(train_data, test_data, max_features)
+        train_df, test_df = apply_bow(train_data, test_data, max_features)
 
         save_data(train_df, os.path.join("./data", "processed", "train_tfidf.csv"))
         save_data(test_df, os.path.join("./data", "processed", "test_tfidf.csv"))
